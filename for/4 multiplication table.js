1976234a85de504f709cf88236006851c9b63e09
programminghero1 <programming.hero1@gmail.com>
2024-01-22 21:23:48 -0600

    added for tasks

---
+++ for/4 multiplication table.js
@@ -0,0 +1,8 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+
+/*programming hero*/
---
+++ for/5 max.js
@@ -0,0 +1,8 @@
+/***
+
+Write JavaScript code that takes an array of numbers as input and returns the maximum element in the array. Use a while loop to iterate through the array and compare each element to find the maximum.
+
+ */
+
+
+/*programming hero*/
